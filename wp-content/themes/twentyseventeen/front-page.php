6238ffc16c64318a4c5523fc073e872297de9faa
James Anderson <jsondev1.0@outlook.com>
2018-07-14 22:58:45 -0400

    testing deploy

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -16,6 +16,7 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+	<h1>test</h1>
 
 		<?php // Show the selected frontpage content.
 		if ( have_posts() ) :
